d6d90647abb4364f47199a18e4197d680e5d043e
Luis <lgarciiaruiiz@gmail.com>
2023-01-05 20:47:06 -0600

    video 30 hibernate CRUD

--- Base+Hibernate/Base+Hibernate/Base Hibernate/src/main/java/com/springsimplespasos/hibernate/pruebas/HibernateMain.java
+++ Base+Hibernate/Base+Hibernate/Base Hibernate/src/main/java/com/springsimplespasos/hibernate/pruebas/HibernateMain.java
@@ -1,5 +1,6 @@
 package com.springsimplespasos.hibernate.pruebas;
 
+import com.springsimplespasos.hibernate.entidades.SimpleEntity;
 import com.springsimplespasos.hibernate.util.HibernateUtil;
 import org.hibernate.Session;
 
@@ -7,6 +8,38 @@ public class HibernateMain {
 
     public static void main(String[] args) {
         Session session = HibernateUtil.getSeccionSessionFactory().openSession();
+        session.beginTransaction();
+        //insertar
+
+        /*SimpleEntity entity = new SimpleEntity();
+        entity.setNombre("Descripcion");
+        session.save(entity);*/
+
+        //actualizar
+        /*SimpleEntity entity = new SimpleEntity();
+        entity.setCodigo(2);
+        entity.setNombre("Descripcion update");
+        session.update(entity);*/
+
+        //eliminar
+        /*SimpleEntity entity = new SimpleEntity();
+        entity.setCodigo(1);
+        session.delete(entity);*/
+
+        /*SimpleEntity entityRead = session.get(SimpleEntity.class,1);
+        System.out.println(entityRead.getCodigo());*/
+
+        SimpleEntity entity = new SimpleEntity();
+        entity.setNombre("Descripcion");
+
+        SimpleEntity entityU = new SimpleEntity();
+        entityU.setCodigo(1);
+        entityU.setNombre("Descripcion update");
+
+       session.saveOrUpdate(entityU);
+        session.saveOrUpdate(entity);
+
+        session.getTransaction().commit();
         session.close();
     }
 
