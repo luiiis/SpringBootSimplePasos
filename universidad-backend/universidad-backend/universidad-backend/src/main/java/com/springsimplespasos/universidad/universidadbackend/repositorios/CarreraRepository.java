07db892d378182897b06b841f3dd257e06e6cbf5
Luis <lgarciiaruiiz@gmail.com>
2023-01-20 17:38:59 -0600

    v49 consultas a traves de metodos

--- universidad-backend/universidad-backend/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/repositorios/CarreraRepository.java
+++ universidad-backend/universidad-backend/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/repositorios/CarreraRepository.java
@@ -5,11 +5,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CarreraRepository extends CrudRepository<Carrera, Integer> {
-
-    @Query("select c from Carrera c where c.nombre like %?1%")
+/*CONSULTAS A TRAVES DE METODOS EN DONDE LA CONSULTA ES IGUAL QUE LA LLAMDA DEL METODO*/
+    //@Query("select c from Carrera c where c.nombre like %?1%")
     Iterable<Carrera> findCarrerasByNombreContains(String nombre);
-    @Query("select c from Carrera c where upper(c.nombre) like upper(%?1%)")
+    //@Query("select c from Carrera c where upper(c.nombre) like upper(%?1%)")
     Iterable<Carrera> findCarrerasByNombreContainsIgnoreCase(String nombre);
-    @Query("select c from Carrera c where c.cantidadAnios > ?1")
+    //@Query("select c from Carrera c where c.cantidadAnios > ?1")
     Iterable<Carrera> findCarrerasByCantidadAniosAfter(Integer cantidadAnios);
 }
