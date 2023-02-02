cbedd3a00bf37ceb59978ca93bb8feaa2ac4426b
luis <lgarciiaruiiz@gmail.com>
2023-02-02 03:09:13 +0100

    responseEntity en GenericController

--- universidad-backend-web/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/GenericController.java
+++ universidad-backend-web/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/GenericController.java
@@ -2,9 +2,12 @@ package com.springsimplespasos.universidad.universidadbackend.controlador;
 
 import com.springsimplespasos.universidad.universidadbackend.exception.BadRequestException;
 import com.springsimplespasos.universidad.universidadbackend.servicios.contratos.GenericoDAO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GenericController <E,S extends GenericoDAO<E>> {
 
@@ -15,12 +18,20 @@ public class GenericController <E,S extends GenericoDAO<E>> {
         this.service = service;
     }
     @GetMapping
-    public List<E>obtenerTodos(){
+    public ResponseEntity<?>obtenerTodos(){
+
+        Map<String,Object> mensaje = new HashMap<>();
         List<E> listado = (List<E>) service.findAll();
+
         if(listado.isEmpty()){
-            throw new BadRequestException(String.format("No se han encontrado %ss "+nombreEntidad));
+            //throw new BadRequestException(String.format("No se han encontrado %ss "+nombreEntidad));
+            mensaje.put("sucess",Boolean.FALSE);
+            mensaje.put("menssaje",String.format("No existen %ss",nombreEntidad));
+            return ResponseEntity.badRequest().body(mensaje);
         }
-        return listado;
+        mensaje.put("sucess",Boolean.TRUE);
+        mensaje.put("datos",listado);
+        return ResponseEntity.ok(mensaje);
     }
     //obtenerPorID(id
 
