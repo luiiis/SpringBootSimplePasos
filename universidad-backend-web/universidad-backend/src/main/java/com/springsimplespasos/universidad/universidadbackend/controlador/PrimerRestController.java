59bcc5865d760384f2a303c186212f075a1a6c6f
luis <lgarciiaruiiz@gmail.com>
2023-02-02 03:02:08 +0100

    response entity

--- universidad-backend-web/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/PrimerRestController.java
+++ universidad-backend-web/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/PrimerRestController.java
@@ -1,5 +1,7 @@
 package com.springsimplespasos.universidad.universidadbackend.controlador;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/restapi")
 public class PrimerRestController {
     @GetMapping("hola-mundo")
-    public String holaMundo(){
-        return "Hola Mundo";
+    public ResponseEntity<String> holaMundo(){
+        return new ResponseEntity<>("Hola Mundo", HttpStatus.ACCEPTED);
     }
 }
