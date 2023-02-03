d8768cc9991ca719f3571b77d8a946af3d8c3966
luis <lgarciiaruiiz@gmail.com>
2023-02-03 04:14:46 +0100

    usando lombok en una clase DTO

--- universidad-backend-web/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/modelo/dto/CarreraDTO.java
+++ universidad-backend-web/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/modelo/dto/CarreraDTO.java
@@ -1,5 +1,14 @@
 package com.springsimplespasos.universidad.universidadbackend.modelo.dto;
 
+import lombok.*;
+
+/*@Setter
+@Getter
+@ToString
+@EqualsAndHashCode*/
+@Data//tiene todos los anteriores
+@NoArgsConstructor
+@AllArgsConstructor
 public class CarreraDTO {
 
     private Integer codigo;
@@ -7,38 +16,5 @@ public class CarreraDTO {
     private Integer cantidad_materias;
     private Integer cantidad_anios;
 
-    public CarreraDTO() {
-    }
 
-    public Integer getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(Integer codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Integer getCantidad_materias() {
-        return cantidad_materias;
-    }
-
-    public void setCantidad_materias(Integer cantidad_materias) {
-        this.cantidad_materias = cantidad_materias;
-    }
-
-    public Integer getCantidad_anios() {
-        return cantidad_anios;
-    }
-
-    public void setCantidad_anios(Integer cantidad_anios) {
-        this.cantidad_anios = cantidad_anios;
-    }
 }
