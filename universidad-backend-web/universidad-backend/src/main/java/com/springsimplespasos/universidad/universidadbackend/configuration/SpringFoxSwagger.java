b6762d703dbdc545f210b2cc0bff7e79da2b6b99
luis <lgarciiaruiiz@gmail.com>
2023-02-08 02:59:19 +0100

    Iniciando con Swagger

---
+++ universidad-backend-web/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/configuration/SpringFoxSwagger.java
@@ -0,0 +1,10 @@
+package com.springsimplespasos.universidad.universidadbackend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxSwagger {
+    //http://localhost:9081/api/v2/universidad/swagger-ui/
+}
--- universidad-backend-web/universidad-backend/target/generated-sources/annotations/com/springsimplespasos/universidad/universidadbackend/modelo/mapper/mapstruct/AlumnoMapperImpl.java
+++ universidad-backend-web/universidad-backend/target/generated-sources/annotations/com/springsimplespasos/universidad/universidadbackend/modelo/mapper/mapstruct/AlumnoMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-02-04T03:56:02+0100",
+    date = "2023-02-08T02:56:04+0100",
     comments = "version: 1.4.1.Final, compiler: javac, environment: Java 11.0.18 (Oracle Corporation)"
 )
 @Component
