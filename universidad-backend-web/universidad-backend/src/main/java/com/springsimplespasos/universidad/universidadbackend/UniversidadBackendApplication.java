b30325f5c07877fdbc7fdb9381a2ff327fc652fe
luis <lgarciiaruiiz@gmail.com>
2023-02-08 04:22:20 +0100

    generar un en tipo war

--- universidad-backend-web/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/UniversidadBackendApplication.java
+++ universidad-backend-web/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/UniversidadBackendApplication.java
@@ -8,12 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
 @SpringBootApplication
-public class UniversidadBackendApplication {
+public class UniversidadBackendApplication extends SpringBootServletInitializer {
 
 
 	public static void main(String[] args) {
