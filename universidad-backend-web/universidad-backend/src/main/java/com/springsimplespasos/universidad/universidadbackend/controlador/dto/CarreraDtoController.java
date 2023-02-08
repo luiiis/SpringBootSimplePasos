1f85f1c50a9eee10c71cf33dc12de0a891925576
luis <lgarciiaruiiz@gmail.com>
2023-02-08 04:04:02 +0100

    Customizando swager -Modelo

--- universidad-backend-web/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/dto/CarreraDtoController.java
+++ universidad-backend-web/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/dto/CarreraDtoController.java
@@ -8,10 +8,7 @@ import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -49,6 +46,11 @@ public class CarreraDtoController extends GenericDtoController<Carrera,CarreraDA
     @GetMapping("/{id}")
     @ApiOperation(value = "Consultar carrera por codigo")
     public ResponseEntity<?> obtenerCarreraPorId(@PathVariable @ApiParam(name = "codigo del sistema") Integer id){
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping
+    public ResponseEntity<?> altaCarrera(@RequestBody @ApiParam(name="Carrera de la universidad") CarreraDTO carreraDTO){
         return ResponseEntity.ok("");
     }
 }
--- universidad-backend-web/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/modelo/dto/CarreraDTO.java
+++ universidad-backend-web/universidad-backend/src/main/java/com/springsimplespasos/universidad/universidadbackend/modelo/dto/CarreraDTO.java
@@ -1,5 +1,7 @@
 package com.springsimplespasos.universidad.universidadbackend.modelo.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
@@ -14,16 +16,20 @@ import javax.validation.constraints.Size;
 @Data//tiene todos los anteriores
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(description = "carrera de la universidad",value = "Carrera",reference = "Carrera")
 public class CarreraDTO {
-    @NotNull
+    @ApiModelProperty(name="codigo del sistema",example = "5")
     private Integer codigo;
     @NotEmpty(message="Debe de ingresar un valor")
     @Size(min=0,max = 80)
+    @ApiModelProperty(name="nombre de la carrera",example = "lic eb Alimentos",required = true)
     //@Pattern(regexp = "")
     //@javax.validation.constraints.
     private String  nombre;
     @Positive(message = "El valor no puede ser negativo")
+    @ApiModelProperty(name="cantidad de materias de toda la carrera",example = "55",required = true)
     private Integer cantidad_materias;
+    @ApiModelProperty(name="cantidad de años de duracion de la carrera",example = "5",required = true)
     private Integer cantidad_anios;
 
 
